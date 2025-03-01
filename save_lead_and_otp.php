c87969f93a46312982de87b0a8b0e0da6abb30b6
Naufal Falah <naufalfallah@gmail.com>
2025-03-01 19:30:18 +0700

    Feature: Add filter

--- save_lead_and_otp.php
+++ save_lead_and_otp.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'helper_2chat.php';
+require_once 'lead_filter.php';
 
 $host = 'localhost';
 // $dbname = 'dbhnskgiz5vgt0';
@@ -35,6 +36,21 @@ $phone = isset($_POST['phone']) ? $_POST['phone'] : null;
 $email = isset($_POST['email']) ? $_POST['email'] : null;
 
 $otp = mt_rand(100000, 999999);
+
+if (is_fake_name($name)) {
+    echo json_encode(['success' => false, 'message' => 'Invalid name', 'error' => 'name']);
+    exit();
+}
+
+if (is_fake_phone($phone)) {
+    echo json_encode(['success' => false, 'message' => 'Invalid phone number', 'error' => 'phone']);
+    exit();
+}
+
+if (is_fake_email($email)) {
+    echo json_encode(['success' => false, 'message' => 'Invalid email address', 'error' => 'email']);
+    exit();
+}
 
 try {
     $stmt = $pdo->prepare("INSERT INTO users (household, citizenship, requirement, household_income, ownership_status, private_property_ownership, first_time_applicant, name, email, phone) 
